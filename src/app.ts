cb1cb971cef83de1dc18da82a5bd0fae3c71c163
Naimur <m.naimur61@gmail.com>
2023-12-21 16:39:35 +0600

    api route error handler

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import express from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
+import router from "./app/routes";
+import httpStatus from "http-status";
+// import globalErrorHandler from "./app/middleware/globalErrorHandler";
 
-const app = express();
+const app: Application = express();
 
 // Cors middleware
 app.use(cors());
@@ -10,8 +13,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-	res.send("Hello Cow!");
+app.use("/api/v1/", router);
+
+// app.use(globalErrorHandler);
+
+// Api route error handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+	res.status(httpStatus.NOT_FOUND).json({
+		success: false,
+		message: "Not Found!",
+		errorMessage: {
+			path: req.path,
+			message: "Api Not Found!",
+		},
+	});
 });
 
 export default app;
